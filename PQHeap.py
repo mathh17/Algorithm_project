08231a1f00070c0b97946af6b0650346624f2fd4
Mathias <mathh17@student.sdu.dk>
2021-03-02 15:30:13 +0100

    Funktioner indsat

--- PQHeap.py
+++ PQHeap.py
@@ -5,4 +5,39 @@ Created on Tue Mar  2 14:58:53 2021
 @author: oeste
 """
 
-aslkdhlkashdlhk
+def extractMin(A):
+    
+    
+
+def insert(A):
+    
+    
+    
+def createEmptyPQ():
+    
+    
+    
+    
+def parent(i):
+    
+    
+    
+def min_Heapify(A,i):
+    
+    
+def heap_MExtract_Min(A):
+    
+    
+def heap_Min(A):
+    
+    
+    
+def heap_increase_Key(A,i,key):
+    
+    
+    
+    
+def min_heap_insert(A,key):
+    
+    
+    
