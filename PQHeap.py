01373a4dd3543d61619ace4f0563b84667d3d47c
Mathias <mathh17@student.sdu.dk>
2021-03-04 15:11:38 +0100

    implementering af mathias methoder

--- PQHeap.py
+++ PQHeap.py
@@ -19,17 +19,29 @@ def createEmptyPQ():
     
 # Mathias  
 def parent(i):
+    int(i/2)
     
 # Mathias
 def left(i):
+    return 2*i
     
 # Mathias    
 def right(i):
+    return 2*i+1
     
     
 # Mathias
 def min_Heapify(A,i):
-    
+    l = left(i)
+    r = right(i)
+    if(l >= A and A[l] < A[i]):
+        least = l
+    else least = i
+    if r >= A and A[r] < A[least]:
+        least = r
+    if least != i:
+        A[i],A[least] = A[least],A[i]
+        min_Heapify(A,least)
     
 def heap_MExtract_Min(A):
     
